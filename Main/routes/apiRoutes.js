a0785449e578779e8162fc9500025bf78cef734f
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-30 23:37:04 -0700

    utilized what was needed from db/log.js for apiRoutes.js to function properly

--- Main/routes/apiRoutes.js
+++ Main/routes/apiRoutes.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
 const log = require('../db/log');
 
-router.get('/', (req, res) => {
-//need to get the note and return it in json and catch any errors
+router.get('/notes', (req, res) => {
+    log 
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
 });
 
-router.post('/', (req, res) => {
-//need to post the note req and res and catch any errors
+router.post('/notes', (req, res) => {
+    log
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
 });
 
-router.delete('/', (req, res) => {
-//extra points if can delete the note
+router.delete('/notes/:id', (req, res) => {
+    log
+      .deleteNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
