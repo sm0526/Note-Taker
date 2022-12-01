a1459f060f0e29e641d28ea0a8f251a5222cb127
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-30 23:50:59 -0700

    added lines 3 and 11 on server.js to correct error of not being able to find api

--- Main/server.js
+++ Main/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
 app.use('/', routes);
 
 
